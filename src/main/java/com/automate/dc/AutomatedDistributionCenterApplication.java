f8153c2c47a9380ed072ccdf879965b65fcaf837
donplex <donpiyumal907@gmail.com>
2020-07-03 01:17:22 +0530

    First Commit For Starter Project

---
+++ src/main/java/com/automate/dc/AutomatedDistributionCenterApplication.java
@@ -0,0 +1,13 @@
+package com.automate.dc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedDistributionCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedDistributionCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/automate/dc/AutomatedDistributionCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automate.dc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedDistributionCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
