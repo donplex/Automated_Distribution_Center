13ad4e3c6196b21f809e52c35e659508684bc5a2
donplex <donpiyumal907@gmail.com>
2020-07-03 02:01:36 +0530

    Edited

--- src/main/java/com/automated/dc/controller/AutomatedDistributionCenterController.java
+++ src/main/java/com/automated/dc/controller/AutomatedDistributionCenterController.java
@@ -1,5 +1,12 @@
 package com.automated.dc.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Product")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AutomatedDistributionCenterController {
 
 }
